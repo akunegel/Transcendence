1a86473a5f6db513c192fa2ed35ce3df742b3a69
Manoug Pfennig <mapfenni@c1r1p8.42nice.fr>
2024-11-16 18:23:18 +0100

    started CustomGameForm to create custom party; Added css styling to CustomGameForm; Added CustomGameForm to /lobby;

--- srcs/front/frontend/src/pages/Lobby/Lobby.jsx
+++ srcs/front/frontend/src/pages/Lobby/Lobby.jsx
@@ -1,18 +1,16 @@
-import React, {useEffect} from 'react'
-import {useNavigate, useLocation} from "react-router-dom"
+import React, {useEffect, useState} from 'react'
+import {useNavigate} from "react-router-dom"
+import CustomGameForm from "../../components/CustomGameForm/CustomGameForm.jsx"
 import logo from "../../assets/logo_lobby.png"
 import styles from "./Lobby.module.css"
 
 
 function Lobby(){
 	const navigate = useNavigate();
+	const [openCustom, setOpenCustom] = useState(false);
 
 	const handleQuick = () => {
 		navigate("/join");
-	}
-
-	const handleCustom = () => {
-
 	}
 
 	const handleStats = () => {
@@ -32,7 +30,8 @@ function Lobby(){
 			<div className={styles.main_container}>
 				<div className={styles.centered_container}>
 					<button onClick={() => handleQuick()}>QUICK JOIN</button>
-					<button onClick={() => handleCustom()}>CUSTOM GAME</button>
+					<button onClick={() => setOpenCustom(openCustom ? false : true)}>CUSTOM GAME</button>
+					{openCustom && <CustomGameForm/>}
 					<button onClick={() => handleStats()}>STATS</button>
 					<button onClick={() => handleReturn()}>RETURN</button>
 					<br/>
